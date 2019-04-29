0603b0cfbd45d98b820779ae03a0caeddd288e19
pricilla gumato sharamo <gumatopricilla22@gmail.com>
2019-04-29 17:03:03 +0300

    Create a email template to welcome the user

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    SUBJECT_PREFIX = 'One Minute Pitch'
+    SUBJECT_PREFIX = 'Personal Blog'
     SENDER_EMAIL = 'gumatopricilla22@gmail.com'
 
     # simple mde  configurations

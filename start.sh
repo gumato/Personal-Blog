1d100ca10873f4da2a987da905327abdd9d65d8c
pricilla gumato sharamo <gumatopricilla22@gmail.com>
2019-05-01 14:31:14 +0300

    deploy

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
-export SECRET_KEY='GUMESHI1'
-export MAIL_USERNAME='gumatopricilla22@gmail.com'
-export MAIL_PASSWORD='715385641'
+export SECRET_KEY=GUMESHI1
+export MAIL_USERNAME=gumatopricilla22@gmail.com
+export MAIL_PASSWORD=715385641
+
 python3.6 manage.py server
